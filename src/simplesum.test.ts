72f0a77c6a65b0ce8b286add76a43ab5a906cee7
italosantana <techitalosantana@gmail.com>
2022-06-14 07:58:27 -0300

    add: new test

--- src/simplesum.test.ts
+++ src/simplesum.test.ts
@@ -2,10 +2,14 @@ import sum from "./sum";
 
 describe("Math", () => {
     it("Should be able to sum A and B", () => {
+        expect(sum(2, 3)).toBe(5);
+    });
+
+    it("Should be able to sum 2 and 3", () => {
         expect(sum(2, 2)).toBe(4);
     });
 
     it("Should be able to sum 2 and 3", () => {
-        expect(sum(2, 3)).toBe(5);
+        expect(sum(2, 1)).toBe(3);
     });
 });
